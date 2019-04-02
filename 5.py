08aa7ea8465be8b76ac62dfda1c4eb48f343c6f6
cocakuma <imfuego@gmail.com>
2019-04-02 18:57:44 +0800

    update

--- 5.py
+++ 5.py
@@ -2,10 +2,8 @@ from urllib import request
 import pickle
 
 data = request.urlopen("http://www.pythonchallenge.com/pc/def/banner.p")
-# print(type(data))
-# str1 = ''.join(str(data))
-# print(pickle.load(data))
 for item in pickle.load(data):
-    print(item)
-    # for x in item:
-
+    line = ""
+    for x in item:
+        line += x[0] * x[1]
+    print(line)
